39ffa4d25fd6ad74bc61c0d0721f9430972b63f5
Ismael Ramiro Valdivia Aranibar <ismael.valdivia@boa.bo>
2020-08-11 20:55:55 -0400

    Updates

---
+++ precache-manifest.7f4395011693668ec7e71f03ef447a9b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99334cd6ed6a2f2b35f713ddf6a18e7",
+    "url": "/LiveModern/index.html"
+  },
+  {
+    "revision": "7b6e0547c1441f055d09",
+    "url": "/LiveModern/static/css/2.75087217.chunk.css"
+  },
+  {
+    "revision": "632f1cabe3c44be8191b",
+    "url": "/LiveModern/static/css/main.1ea0e676.chunk.css"
+  },
+  {
+    "revision": "7b6e0547c1441f055d09",
+    "url": "/LiveModern/static/js/2.39ecf80b.chunk.js"
+  },
+  {
+    "revision": "b9dbed194f4b0677fe3c8f183c879b53",
+    "url": "/LiveModern/static/js/2.39ecf80b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632f1cabe3c44be8191b",
+    "url": "/LiveModern/static/js/main.30e1a2b2.chunk.js"
+  },
+  {
+    "revision": "28522e2e91c2ace10319",
+    "url": "/LiveModern/static/js/runtime-main.048df59b.js"
+  },
+  {
+    "revision": "0d855cb8b8a794706af143547a82ce4c",
+    "url": "/LiveModern/static/media/AvatarEjemplo.0d855cb8.png"
+  },
+  {
+    "revision": "f8292cefed6004156f0775fc58fbde1a",
+    "url": "/LiveModern/static/media/Robot404.f8292cef.png"
+  },
+  {
+    "revision": "85b4fdb40ec1d8776b112c6306c9ce5b",
+    "url": "/LiveModern/static/media/casa1.85b4fdb4.jpg"
+  },
+  {
+    "revision": "8b085b5d8f1dca107fd72357366a03b5",
+    "url": "/LiveModern/static/media/casa2.8b085b5d.jpeg"
+  },
+  {
+    "revision": "250306ad2e437ab5976ecf00241be252",
+    "url": "/LiveModern/static/media/casa3.250306ad.jpg"
+  },
+  {
+    "revision": "5bfece8746059d37a0fbf5c5aa2e8d2f",
+    "url": "/LiveModern/static/media/casa4.5bfece87.jpg"
+  },
+  {
+    "revision": "d5b7655bb6ab6f1a24b6ac537fcb639a",
+    "url": "/LiveModern/static/media/edit_icon.d5b7655b.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/LiveModern/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/LiveModern/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/LiveModern/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/LiveModern/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/LiveModern/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ab25cc426b6fb4135e9d9733ef76da1c",
+    "url": "/LiveModern/static/media/mensaje.ab25cc42.png"
+  },
+  {
+    "revision": "71e37be2c7024f84bd3dc857481a99e7",
+    "url": "/LiveModern/static/media/net.71e37be2.png"
+  }
+]);
